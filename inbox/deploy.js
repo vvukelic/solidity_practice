0612cc1f910bf10a9019bc392b3278f210596738
Vedran Vukelic <vedran.vukelic@gmail.com>
2021-09-18 12:25:58 +0200

    Some more work.

--- inbox/deploy.js
+++ inbox/deploy.js
@@ -1,1 +1,22 @@
-// deploy code will go here
+const HdWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HdWalletProvider(
+  'liar forest slender marriage clown middle actress sense birth catalog uncle rival',
+  'https://kovan.infura.io/v3/af38ea05790c4aa49921ecf37bd84b34'
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+    .send({ from: accounts[0], gas: '1000000' });
+
+  console.log('Contract deployed to', result.options.address);
+};
+deploy();
--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -27,4 +27,10 @@ describe('Inbox', () => {
       const message = await inbox.methods.message().call();
       assert.equal(message, INITIAL_STRING);
   });
+
+  it('can change the message', async () => {
+      await inbox.methods.setMessage('Bye there!').send({ from: accounts[0] });
+      const message = await inbox.methods.message().call();
+      assert.equal(message, 'Bye there!');
+  });
 });
