1adccccf92ead8f6f7f379c560605c1b1ee2db1a
Vedran Vukelic <vedran.vukelic@gmail.com>
2021-09-22 13:07:17 +0200

    Lottery account.

--- lottery/deploy.js
+++ lottery/deploy.js
@@ -15,7 +15,7 @@ const deploy = async () => {
   console.log('Attempting to deploy from account', accounts[0]);
 
   const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+    .deploy({ data: bytecode })
     .send({ from: accounts[0], gas: '1000000' });
 
   console.log('Contract deployed to', result.options.address);
